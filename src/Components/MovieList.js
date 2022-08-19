30724029cbe8c50952ad9275ff23074cafb0e510
kiwiguard <frida.susanne@gmail.com>
2022-08-19 13:20:01 +0200

    movielist design updates

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import placeholderHorizontal from "../Images/placeholder_horizontal.jpg";
 
 const MovieList = ({ movies, title }) => {
   return (
@@ -7,8 +8,13 @@ const MovieList = ({ movies, title }) => {
       {movies.map((movie) => (
         <div className="movie-preview" key={movie._id}>
           <Link to={`/movies/${movie._id}`}>
-            <h2>{movie.title}</h2>
-            <p>Rating {movie.rating}</p>
+            <img
+              className="movie-preview--img"
+              src={placeholderHorizontal}
+              alt="placeholder movieposter"
+            />
+            <h2 className="movie-preview--title">{movie.title}</h2>
+            <p className="movie-preview--rating">{movie.rating}</p>
           </Link>
         </div>
       ))}
